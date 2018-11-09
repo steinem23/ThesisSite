2a2ac5f6e1553945d521098b5be93886859a9c4d
Erin Stein <steinem@appstate.edu>
2018-11-08 19:41:53 -0500

    php syntax errors

--- getMessage.php
+++ getMessage.php
@@ -1,30 +1,31 @@
 <?php
    include ("myCredentials.php");
-   $con = mysqliconnect($servername, $username, $password, $dbname);
+   $con = mysqli_connect($servername, $username, $password, $dbname);
    if(!$con) {
        die("Connection failed: " . mysqli_connect_error());
    }
 
-   $query =  "SELECT * FROM message");
+   $query =  "SELECT uname, message FROM message";
 
    $result = mysqli_query($con, $query);
 
-    echo "<table border = '1'>"
+    echo "<table border = '1'>
     <tr>
         <th>User</th>
         <th>Message</th>
-    </tr>;
+    </tr>";
+
 
 
     while($row = mysqli_fetch_array($result)) {
-        echo "<tr>"
-            echo"<td>" . $row['username'] . "</td>";
-            echo"<td>" . $row['message'] . "</td>";
+        echo "<tr>";
+        echo"<td>" . $row['uname'] . "</td>";
+        echo"<td>" . $row['message'] . "</td>";
         echo "</tr>";    
         
     }
 
-    echo "</table>"
+    echo "</table>";
     mysqli_close($con);
 
    ?>
--- login.php
+++ login.php
@@ -1,17 +1,42 @@
 <?php
    include ("myCredentials.php");
-   $con = mysqliconnect($servername, $username, $password, $dbname);
+   $con = mysqli_connect($servername, $username, $password, $dbname);
    if(!$con) {
        die("Connection failed: " . mysqli_connect_error());
    }
    $userName = $_POST['username'];
-   $password = $POST['password'];
+   $password = $_POST['password'];
 
-   $query =  "SELECT * FROM USERS WHERE uname = " . $userName . " AND pw = " . $password;
 
+    $query = "SELECT fname, lname, uname FROM user WHERE uname='" . $userName . "' AND pw = '" . $password . "';"; 
    $result = mysqli_query($con, $query);
+   
+   echo "<table border = '1'>
+   <tr>
+   <th> First Name</th>
+   <th> Last Name </th>
+   <th> Username </th>
+   </tr>";
+  
+     
+   if (mysqli_num_rows($result) >0) {
+   
+    while ($row = $result->fetch_array()) {
+        echo "<tr>";
+        echo "<td>" . $row['fname'] . "</td>";
+        echo "<td>" .  $row['lname'] . "</td>";
+        echo "<td>" . $row['uname'] . "</td>";
+        echo "</tr>";
+    }
+   }
 
-   //" or 1=1 or " should result in an injection attack
+   else {
+    echo "<p>No user found.</p>";
+   }
+   echo "</table>";
+
+    //   ' or ''='
+   // should result in an injection attack
     mysqli_close($con);
    ?>
 
