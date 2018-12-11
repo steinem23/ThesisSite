0e8a78e2faf0c08348761546c78727a4e46b001d
Erin Stein <steinem@appstate.edu>
2018-12-11 00:52:16 -0500

    organizing

--- Injection/Injection.php
+++ Injection/Injection.php
@@ -208,20 +208,9 @@
 
 
 <!-- Footer -->
-<footer class="w3-container w3-theme-dark w3-padding-16">
-  <h3>Erin Stein</h3>
-  <a href="Thesis.html" class="w3-btn w3-theme-dark">HOME</a><br>
-  <a href="AboutMe.html" class="w3-btn w3-theme-dark">ABOUT ME</a>
-  <div style="position:relative;bottom:px;" class="w3-tooltip w3-right">
-    <span class="w3-text w3-theme-light w3-padding">Go To Top</span>&nbsp;   
-    <a class="w3-text-white" href="#myHeader"><span class="w3-xlarge">
-    <i class="fa fa-chevron-circle-up"></i></span></a>
-  </div>
-   <div style="position:relative;bottom:px;" class="w3-tooltip w3-right">
-    <a href="https://www.linkedin.com/in/erinstein23/" class="fa fa-linkedin"></a>
-  </div>
+<?php include '../Footer.php'; ?>
+<!---->
 
-</footer>
 
 
 <!--Styles-->
