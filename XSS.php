04c2185596e81be6e56868b7a49c53b1f016beb9
Erin Stein <steinem@appstate.edu>
2018-12-11 00:18:47 -0500

    update

--- XSS.php
+++ XSS.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <title>7. Cross-Site Scripting (XSS) </title>
@@ -20,6 +19,7 @@
   <div class="w3-center">
   <h4 class="w3-padding-large">INTRO TO APPLICATION SECURITY</h4>
   <h1 class="w3-xxlarge w3-animate-bottom">CROSS-SITE SCRIPTING (XSS)</h1>
+    </div>
   </div>
 </header>
 <!---->
@@ -47,7 +47,7 @@
       <a href="XEE.html" class="w3-bar-item w3-button">XML External Entities (XXE)</a>
       <a href="BrokenAccessCont.html" class="w3-bar-item w3-button">Broken Access Control</a>
       <a href="SecMisconfig.html" class="w3-bar-item w3-button">Security Misconfiguration</a>
-      <a href="XSS.php" class="w3-bar-item w3-button">Cross-Site Scripting (XSS)</a>
+      <a href="XSS.html" class="w3-bar-item w3-button">Cross-Site Scripting (XSS)</a>
       <a href="InsecureDeserialization.html" class="w3-bar-item w3-button">Insecure Desserialization</a>
       <a href="UsingBadComponents.html" class="w3-bar-item w3-button">Using Components With Known Vulnerabilities</a>
       <a href="InsuffLog&Monitor.html" class="w3-bar-item w3-button">Insufficient Logging and Monitoring </a>
@@ -65,154 +65,224 @@
 <!--BODY INFO-->
 
 
-<div class="w3-padding w3-display-container">
+<div class="w3-container">
 
 <h2> OWASP Top 10 </h2>
   <h3> 7. Cross-Site Scripting (XSS) </h3>
-
-
-       <p w3-class = "w3-large"> Cross-site scripting is a type of injection where untrusted data is not validated allowing for remote code execution of the user’s browser. When directed to a new browser, the updated page can be filled with user-supplied data. An attacker can then hijack an account, redirect the browser, show fraudulent content, or deliver malware to the user which compromises the user-host relationship.  In an advanced cross-site scripting attack, an attacker can simulate anything the victim can do through their browser. This vulnerability has a 33% likelihood of occurring. </p> 
-
-
+    <p> Cross-site scripting is a type of injection where untrusted data is not validated  allowing for remote code execution of the user's browser. Developers can protect their site with server-side validation and escaping. Developers must know how and when to apply the proper sanitization solutions.</p>
 </div>
 <hr>
 <!---->
 
+<!--Table Connections-->
 
-<!--Post to Board Board-->
-        
-        
-        
-<div class="w3-row-padding">
+<?php
+   include ("myCredentials.php");
+   $con = mysqli_connect($servername, $username, $password, $dbname);
+   if(!$con) {
+       die("Connection failed: " . mysqli_connect_error());
+   }
+   
+   $query = "SELECT uname, message FROM message;";
+   $result = mysqli_query($con, $query);
+?>   
+ 
+<!---->
+
+
+
+
+
+
+
+
+<!--POST A MESSAGE-->
+
+<div id = "login"  class="w3-row-padding">
 <div class="w3-half">
-    <form class="w3-container w3-card-4">
-        <h2>Post a Message</h2>
-            <p> type what to do</p>
-    <div class="w3-section">      
-        <input class="w3-input" type="text" name = "username" required>
-        <label>Username</label>
-    </div>
-    
-    <div class="w3-section">      
-        <input class="w3-input" type="text" name = "message" required>
-        <label>Message</label>
-    </div>
-    
-    <div>
-        <button class="w3-btn w3-theme" method = "POST" onclick = "postMSG()">Post</button> <br>
-        <hr>
-    </div>
-    </form>
-</div>
+<form action = "postMessage.php" class="w3-container w3-card-4">
+  <h2>Post a Message</h2>
+  <p> Instructions for writing script to message board.</p>
+  <div class="w3-section">      
+    <input class="w3-input" type="text" id="username" required>
+    <label>Username</label>
+  </div>
+  <div class="w3-section">      
+    <input class="w3-input" type="text" id="message" required>
+    <label>Message</label>
+  </div>
+  <div>
+    <button class="w3-btn w3-theme" type="button" onclick = "postMessage()">Post</button> <br>
+ <br> 
+    <button class="w3-btn w3-theme" type="button" onclick = "clearTable();">Clear Board</button> <br>
+ <br> 
+    <button class="w3-btn w3-theme" type="button" onclick = "showTable();">Show Board</button> <br>
+    <hr>
+
+  </div>
+ </form>
+ </div>
+ <!---->
 
 
 
 
 
-
-
-
-
-<div class="w3-half">
-    <form class="w3-container w3-card-4">
-        <h2>Message Board</h2>
-            <p>See all messages here.</p>
-   
-   
-   
+<!--MESSAGE BOARD-->
+<div id = "toReplace" class="w3-half">
+    <form class = "w3-container w3-card-4">
+    <h2>Message Board</h2>
+        <p>See all messages here.</p>
+        
     <div class="w3-container">
     <hr>
-    <div class="w3-responsive w3-card-4">
-        <table class="w3-table w3-striped w3-bordered">
+    <div class = "w3-responsive w3-card-4">
+        <table class= "w3-table w3-striped w3-bordered">
             <thead>
                 <tr class="w3-theme">
-                <th>Username</th>
-                <th>Message</th>
-                </tr>
-            </thead>
-            
-            <tbody> 
-            
+                 <th>Username</th>
+                 <th>Message </th>
+                 </tr>
+             </thead>
+             <tbody>
                 <?php
-                    include("myCredentials.php");
-                    $con = mysqli_connect($servername, $username, $password, $dbname);
-                    if(!$con) {
-                        die("Connection Failed: " . mysqli_connect_error());
+                    if(mysqli_num_rows($result)>0) {
+                        while($row = $result->fetch_array()) {
+                         echo "<tr>";
+                         echo "<td>" . $row['uname'] . "</td>";
+                         echo "<td>" . $row['message'] . "</td>";
+                         echo "</tr>";
+                            
+                        }
                     }
-        
-                    $query = "SELECT uname, message FROM message";
-                    $result = mysqli_query($con, $query);
-        
+                    ?>
+             </tbody>
+         </table>
+     </div>
+  <hr>
+  </form>
+  </div>
+  </div> 
+
+<!---->
 
 
-                    while($row = $result->fetch_array()) {
-                        echo "<tr>";
-                        echo "<td>" . $row['uname'] . "</td>";
-                        echo "<td>" . $row['message'] . "</td>";
-                        echo "</tr>";
-                    }
+<!--UPDATE MESSAGE BOARD-->
+
+
+
+<!--REPLACE USER INFORMATION -->
+
+      <script>
+         <!--
+            //Browser Support Code
+            function postMessage(){
+               var ajaxRequest;  // The variable that makes Ajax possible!
+               try {
+                  // Opera 8.0+, Firefox, Safari
+                  ajaxRequest = new XMLHttpRequest();
+               }catch (e) {
+                  // Internet Explorer Browsers
+                  try {
+                     ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+                  }catch (e) {
+                     try{
+                        ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                     }catch (e){
+                        // Something went wrong
+                        alert("Your browser broke!");
+                        return false;
+                     }
+                  }
+               }
+               // Create a function that will receive data 
+               // sent from the server and will update
+               // div section in the same page.
                     
-                    mysqli_close($con);
-                ?>
-            </tbody>
-        </table>
-    </div> 
-   
-   
-  <hr> 
+               ajaxRequest.onreadystatechange = function(){
+                  if(ajaxRequest.readyState == 4){
+                     var ajaxDisplay = document.getElementById('toReplace');
+                     ajaxDisplay.innerHTML = ajaxRequest.responseText;
+                  }
+               }
+               
+               // Now get the value from user and pass it to
+               // server script.
+                    
+               var UNs = document.getElementById('username').value;
+               var MSG = document.getElementById('message').value;
+               var queryString = "?username=" + UNs ;
+               queryString +=  "&message=" + MSG;
+               
+               
+               ajaxRequest.open("GET", "postMessage.php" + queryString, true);
+               ajaxRequest.send(); 
+           
+            }
+         //-->
+      </script>
 
 
 
-    <div>
-            <button class="w3-btn w3-theme" onclick="resetBoard()">Reset Board</button> <br>
-    </div>
 
-    <br>    
-
-    </form>
-</div>
-</div>
-
-<script>
-function resetBoard() {
-   <?php
-    include("myCredentials.php");
-    $con = mysqli_connect($servername, $username, $password, $dbname);
-    if (!$con) {
-        die("connection failed: "  . mysqli_connect_error());
-    }
-
-    $reset = "TRUNCATE TABLE message";
-
-    mysqli_query($con, $reset);
-    mysqli_close($con);
-    ?>
+<!---->
 
 
-}
 
 
-function postMSG() {
-    
-   <?php
-    include("myCredentials.php");
-    $con = mysqli_connect($servername, $username, $password, $dbname);
-    if (!$con) {
-        die("connection failed: "  . mysqli_connect_error());
-    }
-    $username = $_GET['username'];
-    $message = $_GET['message'];
-    $posting = "INSERT INTO message VALUES (NULL, '" . $username . "', '" . $message . "');";
 
 
-    mysqli_query($con, $posting);
-    mysqli_close($con);
-    ?>
-}
-
-</script>
+<!--CLEAR MESSAGES TABLE-->
 
 
+
+      <script>
+         <!--
+            //Browser Support Code
+            function clearTable(){
+               var ajaxRequest;  // The variable that makes Ajax possible!
+               try {
+                  // Opera 8.0+, Firefox, Safari
+                  ajaxRequest = new XMLHttpRequest();
+               }catch (e) {
+                  // Internet Explorer Browsers
+                  try {
+                     ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+                  }catch (e) {
+                     try{
+                        ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                     }catch (e){
+                        // Something went wrong
+                        alert("Your browser broke!");
+                        return false;
+                     }
+                  }
+               }
+               // Create a function that will receive data 
+               // sent from the server and will update
+               // div section in the same page.
+                    
+               ajaxRequest.onreadystatechange = function(){
+                  if(ajaxRequest.readyState == 4){
+                     var ajaxDisplay = document.getElementById('toReplace');
+                     ajaxDisplay.innerHTML = ajaxRequest.responseText;
+                  }
+               }
+               
+               // Now get the value from user and pass it to
+               // server script.
+                    
+               
+               ajaxRequest.open("GET", ".php" + queryString, true);
+               ajaxRequest.send(); 
+           
+            }
+         //-->
+      </script>
+
+
+
+<!---->
 
 
 
@@ -222,23 +292,24 @@ function postMSG() {
 
 <!--Resources Accordion-->
 <div class="w3-container">
-<hr>
 <h2 class="w3-center">Resources</h2>
-<button onclick="myAccFunc('Demo1')" class="w3-padding-16 w3-theme w3-button w3-block w3-left-align">What Can Developers Do?</button>
+<button onclick="myAccFunc('Demo1')" class="w3-padding-16 w3-theme w3-button w3-block w3-left-align">Hint</button>
 <div id="Demo1" class="w3-hide">
   <div class="w3-container">
-
-    <p>The developer can protect their site from cross-site scripting with server-side validation and escaping. Escaping ensures syntactically significant characters are treated as data and not input characters to the interpreter's parser. This protects users from cross-site scripting by expressing the character while also disregarding their significance in the language. HTML has six significant characters: &, <, >, ", ', and /. These characters are replaced with HTML entity encoding to prevent the execution of these characters as input rather than data. A developer must also know how and when to apply the proper sanitization solution. </p>
-  
-
-</div>
+    <p>Try using script  tags in your message!</p>
+  </div>
 </div>
 <button onclick="myAccFunc('Demo2')" class="w3-padding-16 w3-theme w3-button w3-block w3-left-align"> More Information</button>
 <div id="Demo2" class="w3-hide">
   <a href="https://www.owasp.org/index.php/Top_10-2017_A7-Cross-Site_Scripting_(XSS)" class="w3-button w3-block w3-left-align">OWASP Top 10-2017: Cross-Site Scripting</a>
   <a href="https://www.w3.org/TR/charmod/#sec-Escaping" class="w3-button w3-block w3-left-align">Character Escaping</a>
-    <a href="https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet" class="w3-button w3-block w3-left-align">XSS Prevention Cheat Sheet</a>
+  <a href="#" class="w3-button w3-block w3-left-align">Link 3 for more information about XSS</a>
 </div>
+<button onclick="myAccFunc('Demo3')" class="w3-padding-16 w3-theme w3-button w3-block w3-left-align">How to Protect Against this Vulnerability</button>
+<div id="Demo3" class="w3-hide">
+  <div class="w3-container">
+    <a href="https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet" class="w3-button w3-block w3-left-align">XSS Prevention Cheat Sheet</a>
+  </div>
 </div>
 
 <hr>
@@ -278,7 +349,6 @@ function postMSG() {
   background: #007bb5;
   color: white;
 }
-
 /* Add a hover effect if you want */
 .fa:hover {
     opacity: 0.7;
@@ -291,8 +361,6 @@ function postMSG() {
 
 <!-- Script for Accordions -->
 <script>
-
-
 // Accordions
 function myAccFunc(id) {
     var x = document.getElementById(id);
@@ -302,8 +370,6 @@ function myAccFunc(id) {
         x.className = x.className.replace(" w3-show", "");
     }
 }
-
-
 </script>
 
 </body>
